8c15524159e42dde0db50aa9519645b48c4fb856
Eugene Davidov <14vettona@rambler.ru>
2020-08-04 13:15:58 +0300

    Updates

---
+++ precache-manifest.58b63f7b943108553a1f8dec68c8c106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fe6af24bf274f32aaf6f209b006749",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "a39af7d533722b07b3fd",
+    "url": "/shop/static/css/main.a587eb34.chunk.css"
+  },
+  {
+    "revision": "5cc0f6d3ac2be51d9648",
+    "url": "/shop/static/js/2.2e530b7b.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/shop/static/js/2.2e530b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39af7d533722b07b3fd",
+    "url": "/shop/static/js/main.0698c1b4.chunk.js"
+  },
+  {
+    "revision": "fe3dfe3aa0d1eddb7eb9",
+    "url": "/shop/static/js/runtime-main.f559145c.js"
+  }
+]);
