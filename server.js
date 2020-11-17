97f7bd55e762e95630bf5e5ce683af22e91e0854
Eugene Davidov <14vettona@rambler.ru>
2020-11-17 22:17:03 +0200

    Changed package.json and server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults({
   static: './build',
 });
 
-const PORT = process.env.PORT || 9200;
+const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
