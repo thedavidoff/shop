3fbe54d4f24b24de79f3be53755523dc15277617
Eugene Davidov <14vettona@rambler.ru>
2020-11-17 23:15:49 +0200

    Changed server.js file

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static("build"));
 
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
