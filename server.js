33316dc10757771aa1a4fb5813992eabe3fe6822
Eugene Davidov <14vettona@rambler.ru>
2020-11-17 23:03:38 +0200

    Fixed package.json

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults({
-  static: './build',
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(PORT, () => {
-  console.log('Server is running');
-});
+app.listen(process.env.PORT || 3000);
