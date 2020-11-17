c4b218b50e508fc68632c10c65206b5822d6bfa6
Eugene Davidov <14vettona@rambler.ru>
2020-11-17 23:39:33 +0200

    Changed server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const app = express();
-app.use(express.static("build"));
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({
+  static: './build',
 });
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
